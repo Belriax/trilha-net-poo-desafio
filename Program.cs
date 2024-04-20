9d561718c26167bdf03f2f8da1b24fed651f35b7
Belriax <gleiconsousa@gmail.com>
2024-04-20 10:43:09 -0300

    Desafio de código C# POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia("949870214", "Nokia Windows Phone", "120154/547", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone");
+Smartphone iphone = new Iphone("97457410", "Iphone 15s", "1245781321", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
